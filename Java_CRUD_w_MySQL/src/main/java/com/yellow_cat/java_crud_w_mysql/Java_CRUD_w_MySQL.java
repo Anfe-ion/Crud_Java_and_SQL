6417c2b1e289292ba2eb27dd5951d034f40362c6
Anfe-ion <andres.moncada.c@gmail.com>
2023-07-29 22:03:03 -0500

    Se agrega el diseño JPanel.

---
+++ Java_CRUD_w_MySQL/src/main/java/com/yellow_cat/java_crud_w_mysql/Java_CRUD_w_MySQL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.yellow_cat.java_crud_w_mysql;
+
+/**
+ *
+ * @author Andrés
+ */
+public class Java_CRUD_w_MySQL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Java_CRUD_w_MySQL/src/main/java/com/yellow_cat/java_crud_w_mysql/inicio.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.yellow_cat.java_crud_w_mysql;
+
+/**
+ *
+ * @author Andrés
+ */
+public class inicio {
+    
+}
